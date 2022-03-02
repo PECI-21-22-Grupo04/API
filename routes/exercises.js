bf51163d95eba9ddbbe7a08478c5a526309f05db
diogomaduro97 <diogomaduro97@gmail.com>
2022-03-02 16:49:54 +0000

    query params added

--- routes/exercises.js
+++ routes/exercises.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const Exercise = require('../models/Exercise')
 
 router.get('/', async (req, res) => {
-    const exercises = await Exercise.find();
+    console.log(req.query.object + ' . ')
+    const exercises = await Exercise.find(req.query);
     res.render('exercises', {exercises: exercises});
 });
+
+router.post('/', async (req, res) => {
+    res.redirect('/exercises?difficulty=easy');
+})
 
 /* route.get('/images/:filename', (req, res) => {
 
