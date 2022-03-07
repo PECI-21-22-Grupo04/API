4cf1e1004a3c4259f719af80b0f6baaa5a3a5a2f
adao <filipeadao@ua.pt>
2022-03-07 22:06:26 +0000

    sync

--- routes/plans.js
+++ routes/plans.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Plan = require('../models/Plan')
+const Exercise = require('../models/Exercise')
+
 
 router.get('/', async (req, res) => {
 
@@ -15,6 +17,25 @@ router.get('/', async (req, res) => {
     else {res.render('plans', {plans: {} });
 } 
 });
+router.get('/upload', async (req, res) => {
+    const exercises = await Exercise.find(req.query);
+
+    if (exercises)
+    {
+        res.render('uploadPlan', {exercises})
+    }
+    
+});
+router.post('/upload', (req, res) => {
+    console.log(req.file.path)
+    const plan = new Plan({
+      
+        exerciseList: []
+
+    }).save()
+    .then(() => console.log('Plan created'));
+    res.redirect('/upload');
+})
 
 
 module.exports = router;
