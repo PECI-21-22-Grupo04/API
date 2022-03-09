8f8be20715201e303b673e5fe7acc33b15e4e36a
diogomaduro97 <diogomaduro97@gmail.com>
2022-03-09 03:26:01 +0000

    Front-end Updated

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -6,3 +6,14 @@ menuIcon.addEventListener("click", () => {
     sidebar.classList.toggle("open");
     menuIcon.classList.toggle("change");
 })
+
+
+const clearInput = () => {
+    const input = document.querySelector("[data-input]");
+    input.value = "";
+  }
+  
+  const clearBtn = document.getElementById("clear-btn");
+  clearBtn.addEventListener("click", clearInput);
+  
+  
