ce2fb5049e5c3633d952eb1d818aae5e858a0860
adao <filipeadao@ua.pt>
2022-03-10 02:58:23 +0000

    minor

--- routes/login.js
+++ routes/login.js
@@ -4,4 +4,11 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('login');
 });
+
+
+router.post('/', (req, res) => {
+
+    res.send('todo, with firebase auth');
+});
+
 module.exports = router;
