10707b6381c9e1aa14ca8f3c092c2dcab0888781
adao <filipeadao@ua.pt>
2022-03-12 15:40:43 +0000

    added support for a reg/login , will add backend for it

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/home', (req, res) => {
+    res.render('home');
+});
+
 router.get('/', (req, res) => {
-    res.render('home', {teste: 'abc'});
+    res.render('login');
+});
+
+
+router.get('/reg', (req, res) => {
+    res.send('will add a reg page soon')
 });
 module.exports = router;
